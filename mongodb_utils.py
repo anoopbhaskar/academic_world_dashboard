81cff49f140a83f288499d659b659b565f3db1f9
Anoop Bhaskar <102567446+anoopbhaskar@users.noreply.github.com>
2025-08-08 17:22:07 -0700

    Update mongodb_utils.py

--- mongodb_utils.py
+++ mongodb_utils.py
@@ -1,9 +1,15 @@
 from pymongo import MongoClient
+from datetime import datetime
 
 client = MongoClient("mongodb://localhost:27017")
 db = client["academicworld"]
+
 faculty_col = db["faculty"]
 pub_col = db["publications"]
+favorites_col = db["favorites"]
+profiles_col = db["user_profiles"]
+
+
 
 def get_all_universities():
     return sorted(faculty_col.distinct("affiliation.name"))
@@ -17,15 +23,33 @@ def get_all_faculty_names():
 def get_faculty_by_name(name):
     return faculty_col.find_one({"name": name})
 
+def get_faculty_by_id(fid):
+    return faculty_col.find_one({"id": fid})
+
+
+
 def get_publications_by_ids(pub_ids, limit=5):
     return list(pub_col.find({"id": {"$in": pub_ids}}).sort("numCitations", -1).limit(limit))
 
+def get_publication_counts_by_keyword(keyword, start_year):
+    pipeline = [
+        {"$match": {
+            "keywords.name": keyword,
+            "year": {"$gte": start_year}
+        }},
+        {"$group": {
+            "_id": "$year",
+            "count": {"$sum": 1}
+        }},
+        {"$sort": {"_id": 1}}
+    ]
+    results = list(pub_col.aggregate(pipeline))
 
-from datetime import datetime
+    years = list(range(start_year, 2025))
+    year_counts = {r["_id"]: r["count"] for r in results}
+    return [year_counts.get(year, 0) for year in years], years
 
-client = MongoClient("mongodb://localhost:27017/")
-db = client["academicworld"]
-favorites_col = db["favorites"]
+
 
 def save_to_favorites(user, faculty_id):
     if not favorites_col.find_one({"user": user, "faculty_id": faculty_id}):
@@ -38,11 +62,47 @@ def save_to_favorites(user, faculty_id):
 def get_favorites(user):
     return list(favorites_col.find({"user": user}))
 
-def get_faculty_by_id(fid):
-    return faculty_col.find_one({"id": fid})
+def remove_from_favorites(user, faculty_id):
+    favorites_col.delete_one({"user": user, "faculty_id": faculty_id})
 
 def clear_favorites(user):
     favorites_col.delete_many({"user": user})
 
-def remove_from_favorites(user, faculty_id):
-    favorites_col.delete_one({"user": user, "faculty_id": faculty_id})
+
+def save_user_profile(user, first_name, last_name, email):
+    profiles_col.update_one(
+        {"user": user},
+        {"$set": {
+            "first_name": first_name,
+            "last_name": last_name,
+            "email": email
+        }},
+        upsert=True
+    )
+
+def get_user_profile(user):
+    return profiles_col.find_one({"user": user}) or {}
+
+def save_user_interests(user, interests):
+    profiles_col.update_one(
+        {"user": user},
+        {"$set": {"interests": interests}},
+        upsert=True
+    )
+
+def get_user_interests(user):
+    profile = get_user_profile(user)
+    return profile.get("interests", [])
+
+def add_favorite_to_profile(user, faculty_id):
+    profiles_col.update_one(
+        {"user": user},
+        {"$addToSet": {"favorites": faculty_id}},
+        upsert=True
+    )
+
+def remove_favorite_from_profile(user, faculty_id):
+    profiles_col.update_one(
+        {"user": user},
+        {"$pull": {"favorites": faculty_id}}
+    )
